f2ad24e9d091dfccdd18fbdcdc16058edfed89ad
MeccaX1 <lucamecco@gmail.com>
2024-09-20 02:12:41 +0200

    Primo Upload.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         SchermataPrincipale schermataPrincipale = new SchermataPrincipale();
         schermataPrincipale.setVisible(true);
-        schermataPrincipale.setSize(800, 600);
     }
 }
